e0d7789319df304f5b64278199c9849a7fa33088
zaisuf <zaisufmaiyan76@gmail.com>
2026-03-14 06:36:49 +0530

    Move Google Fonts to _document.tsx to fix Next.js warning

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -31,12 +31,6 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="theme-color" content="#E21D24" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
       </Head>
       <Component {...pageProps} />
     </>
